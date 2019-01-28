999eb1f7ff21ef23bfcbee40db891cd028bc5130
ianpas <e78e78e78@126.com>
2019-01-28 18:39:14 +0800

    add auto-format online

---
+++ auto-format/precache-manifest.db150a7e71e4bee6b5d27b3b550d9cb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2def20750145eae0168",
+    "url": "/auto-format/static/js/runtime~main.f2def207.js"
+  },
+  {
+    "revision": "a7ccc4b1e96109b32d89",
+    "url": "/auto-format/static/js/main.a7ccc4b1.chunk.js"
+  },
+  {
+    "revision": "14df3c2a1a146efcf4f8",
+    "url": "/auto-format/static/js/1.14df3c2a.chunk.js"
+  },
+  {
+    "revision": "a7ccc4b1e96109b32d89",
+    "url": "/auto-format/static/css/main.e0f2b183.chunk.css"
+  },
+  {
+    "revision": "49749b749a63f2efc81b2c299d95104d",
+    "url": "/auto-format/index.html"
+  }
+];
