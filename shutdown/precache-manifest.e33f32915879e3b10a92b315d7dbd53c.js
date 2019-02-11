823c7606654bca425096bccd338b5366b0e8c01c
ianpas <e78e78e78@126.com>
2019-02-11 14:02:14 +0800

    upload shutdown site

---
+++ shutdown/precache-manifest.e33f32915879e3b10a92b315d7dbd53c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "43102482a4853cfa65b64e76674a437e",
+    "url": "/shutdown/static/media/logo.43102482.svg"
+  },
+  {
+    "revision": "43102482a4853cfa65b64e76674a437e",
+    "url": "/shutdown/static/media/dragon-48-white.43102482.svg"
+  },
+  {
+    "revision": "9092eed7b513aa577ff2b1e334fc1c9a",
+    "url": "/shutdown/static/media/background.9092eed7.svg"
+  },
+  {
+    "revision": "100b011ac6c8ebb208a1",
+    "url": "/shutdown/static/js/runtime~main.100b011a.js"
+  },
+  {
+    "revision": "47321bf39c48c711b342",
+    "url": "/shutdown/static/js/main.47321bf3.chunk.js"
+  },
+  {
+    "revision": "99e22cf7483edf6c5f9b",
+    "url": "/shutdown/static/js/1.99e22cf7.chunk.js"
+  },
+  {
+    "revision": "47321bf39c48c711b342",
+    "url": "/shutdown/static/css/main.8873fa4e.chunk.css"
+  },
+  {
+    "revision": "d2420563f17cd5b136da147f0ad05da5",
+    "url": "/shutdown/index.html"
+  }
+];
